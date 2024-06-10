f23259b683e4698758fbc4f24f1b53d145477e31
acxelcastillo123 <88935646+acxelcastillo123@users.noreply.github.com>
2024-06-10 18:32:56 -0500

    Add files via upload

---
+++ src/com/aluracursos/conversor/principal/Divisas.java
@@ -0,0 +1,8 @@
+package com.aluracursos.conversor.principal;
+
+public record Divisas(String base_code,
+                      float ARS,
+                      float BRL,
+                      float COP
+                      ) {
+}
